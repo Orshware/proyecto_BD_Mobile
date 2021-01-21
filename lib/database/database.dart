1d3a8e9322ef88b3991addd07572ef8f59c0fe83
reitmas32 <rafa.zamora.rals@gmail.com>
2021-01-21 06:32:51 -0600

    Metodo para la creacion de un cliente y un domicilio

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -2,24 +2,53 @@ import 'dart:convert';
 import 'dart:io';
 import 'dart:math';
 import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:proyecto_BD_Mobile/models/domicilio_cliente.dart';
 
 class DB {
+  static int current_id_cliente;
 
-  static Future<bool> nuevoCliente(
-  String nombre,
-  String ap_Pat,
-  String ap_Mat,
-  String rz_Social,
-  String email,
-  String estado,
-  String colonia,
-  String calle,
-  String numero,
-  String cp,
+  static Future<Domiclio_Cliente> nuevoCliente(
+    String nombre,
+    String ap_pat,
+    String ap_mat,
+    String rz_social,
+    String email,
+    String estado,
+    String colonia,
+    String calle,
+    String numero,
+    String cp,
   ) async {
     Future.delayed(Duration(seconds: 1));
-    
-    return true;
+    var url = 'http://10.0.2.2:3000/cliente/domicilio';
+    var response = await http.post(
+      url,
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(
+        <String, String>{
+          'nombre': nombre,
+          'ap_pat': ap_pat,
+          'ap_mat': ap_mat,
+          'rz_social': rz_social,
+          'email': email,
+          'estado': estado,
+          'colonia': colonia,
+          'calle': calle,
+          'numero': numero,
+          'cp': cp,
+        },
+      ),
+    );
+    var resJson = json.decode(response.body);
+    var dom_clt = Domiclio_Cliente(
+      id_cliente: resJson['id_cliente'],
+      id_domicilio: resJson['id_domicilio'],
+      id_domiclio_cliente: resJson['id_domiclio_cliente'],
+    );
+    DB.current_id_cliente = dom_clt.id_cliente;
+    return dom_clt;
   }
-
 }
